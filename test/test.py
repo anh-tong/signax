5efdf4c5c803a26654fa70dc4f3b41829bd2d68e
Enver Menadjiev <enver1323@gmail.com>
2022-08-19 15:09:58 +0900

    Modify setup

--- test/test.py
+++ test/test.py
@@ -1,20 +1,4 @@
 import torch
-from signax import _impl
+from signax import cpu_ops
 
-print(dir(_impl.cpuSignatureForward()))
-# from typing import List
-#
-#
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         n = len(nums)
-#         index_map = dict(zip(nums, range(n)))
-#         res = []
-#
-#         for i in range(n):
-#             for j in range(i, n):
-#                 target = - (i + j)
-#                 if target in index_map and index_map[target] > j:
-#                     res.append([nums[i], nums[j], nums[target]])
-#
-#         return res
+print(dir(cpu_ops))
