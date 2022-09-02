941099d66bfefc871b00b2b524e96c38d933f555
anh tong <anhth@unist.ac.kr>
2022-09-02 13:15:00 +0900

    fix index_select test

--- test/test_utils.py
+++ test/test_utils.py
@@ -8,12 +8,12 @@ def test_index_select():
     dim = 4
     a = jnp.arange(0, dim**2).reshape((dim, dim))
     indices = jnp.array([[0, 0], [0, 1], [3, 3]])
-    true_output = jnp.array([0, 4, 15])
+    true_output = jnp.array([0, 1, 15])
     assert jnp.allclose(true_output, index_select(a, indices))
 
     # second test
     dim = 3
     a = jnp.arange(0, dim**3).reshape((dim, dim, dim))
     indices = jnp.array([[0, 0, 0], [0, 1, 1], [2, 2, 2]])
-    true_output = jnp.array([0, 12, 26])
+    true_output = jnp.array([0, 4, 26])
     assert jnp.allclose(index_select(a, indices), true_output)
