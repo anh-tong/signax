b55ed80a1dacc830dfb32a489ae3e3905adabe34
anh tong <anhth@unist.ac.kr>
2022-09-23 13:54:54 +0900

    finalize Window Adjusted and refactor

--- examples/utils/brownian_motion.py
+++ examples/utils/brownian_motion.py
@@ -1,19 +1,25 @@
 import jax
 import jax.numpy as jnp
-from jax._src.random import PRNGKey
+from jax.random import PRNGKey
 
 
-def get_bm_noise(random_key: PRNGKey, n_points: int, num_samples: int = 1000):
+def get_bm_noise(
+    random_key: PRNGKey,
+    n_points: int,
+    num_samples: int = 1000,
+):
     """Generate examples of a Brownian motion."""
     random_keys = jax.random.split(random_key, num_samples)
     paths = jnp.array(
         [gen_bm_noise(random_keys[i], n_points) for i in range(num_samples)]
     )
-    y = jnp.zeros_like(paths[:, 0, :-1])
-    return paths, y
+    return paths
 
 
-def gen_bm_noise(random_key: PRNGKey, n_points: int = 100):
+def gen_bm_noise(
+    random_key: PRNGKey,
+    n_points: int = 100,
+):
     dt = 1 / jnp.sqrt(n_points)
     nd = jax.random.normal(random_key, (n_points - 1,)).cumsum()
     bm = dt * jnp.r_[0.0, nd]
