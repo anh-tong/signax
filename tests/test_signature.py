c42ab29dab0ad5a2cc65a9a309a6ae6627941af2
anh tong <anhth@unist.ac.kr>
2023-06-02 18:08:16 +0900

    add logsignature test and fix previous tests

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -8,6 +8,7 @@ import torch
 from numpy.random import default_rng
 
 from signax import (
+    logsignature,
     multi_signature_combine,
     signature,
     signature_batch,
@@ -108,6 +109,26 @@ def test_signature(stream):
 
     torch_path = torch.tensor(path)
     torch_signature = signatory.signature(
+        torch_path[None, ...], depth=depth, stream=stream
+    )
+    torch_signature = jnp.array(torch_signature.numpy())
+
+    assert jnp.allclose(jax_signature, torch_signature)
+
+
+@pytest.mark.parametrize("stream", [True, False])
+def test_logsignature(stream):
+    depth = 3
+    length = 100
+    dim = 5
+
+    path = rng.standard_normal((length, dim))
+    jax_signature = logsignature(path, depth=depth, stream=stream)
+    ravel_fn = jax.vmap(jnp.ravel) if stream else jnp.ravel
+    jax_signature = jnp.concatenate([ravel_fn(x) for x in jax_signature], axis=-1)
+
+    torch_path = torch.tensor(path)
+    torch_signature = signatory.logsignature(
         torch_path[None, ...], depth=depth, stream=stream
     )
     torch_signature = jnp.array(torch_signature.numpy())
